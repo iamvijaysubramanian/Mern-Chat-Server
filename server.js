1b68031734af3cd9c7366fe4e4f9e6826d6157c5
iamvijaysubramanian <vijaysubbu69@gmail.com>
2023-03-23 22:40:07 +0530

    cors

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = require('http').createServer(app);
 const PORT = process.env.PORT || 5001;
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://mern-chatapplication.netlify.app/',
+    origin: '*',
     methods: ['GET', 'POST']
   }
 })
